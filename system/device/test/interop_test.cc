miyu-xu/packages-modules-Bluetooth

Interop fix to not read PPCP for devices that report incompatible values.
Bug: 360314855
Fix: 360314855
Flag: EXEMPT, targeted IOP fix
Test: m Bluetooth, manual testing
Change-Id: I69b13f959351075b45603a514f06a967101cccf4

system/device/test/interop_test.cc
@@ -364,7 +364,9 @@ TEST_F(InteropTest, test_name_hit) {
   EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_AUTH_FOR_HID_POINTING,
                                  "Targus BT Laser Notebook Mouse"));
   EXPECT_TRUE(interop_match_name(INTEROP_REMOVE_HID_DIG_DESCRIPTOR, "Motorola Keyboard KZ500"));
-  EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS, "BSMBB09DS"));
+  EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS, "DXCMog"));
+  EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS, "Dexcom 123"));
+  EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS, "DXC01ab"));
   EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_AAC_CODEC, "abramtek M1"));
   EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_AAC_VBR_CODEC, "Audi_MMI_2781"));
   EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_AVDTP_RECONFIGURE, "KMM-BT51*HD"));

SunOS-Project/android_packages_modules_Bluetooth

CRs-Fixed: 3958355
(cherry picked from commit 99527fc62ec1f891cd53afe909bc48c5a3d710bb)

system/device/test/interop_test.cc
@@ -433,6 +433,9 @@ TEST_F(InteropTest, test_name_hit) {
                                  "Motorola Keyboard KZ500"));
   EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS,
                                  "BSMBB09DS"));
+  EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS, "DXCMog"));
+  EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS, "Dexcom 123"));
+  EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_LE_CONN_PREFERRED_PARAMS, "DX01ab"));
   EXPECT_TRUE(interop_match_name(INTEROP_DISABLE_AAC_CODEC, "abramtek M1"));
   EXPECT_TRUE(
       interop_match_name(INTEROP_DISABLE_AAC_VBR_CODEC, "Audi_MMI_2781"));