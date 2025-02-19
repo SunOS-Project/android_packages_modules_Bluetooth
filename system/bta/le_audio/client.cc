d5a45089a3dfd6de36790fc61bdaf2e420231aca
Satheesh Kumar Pallemoni <quic_spallemo@quicinc.com>
2025-02-19 22:46:09 +0530

    Le-Audio: Update call_audio_route_ to none, when call ended.
    - When Call ended stop sent on decoding session updated ack
      success, and it came before BluetoothInCallservice got update
      of CallRemoved.
    
    - When BluetoothInCallservice got update of callRemoved, then
      SetInCall as alse updated to BT stack.
    
    - When MM sent stop on encoding session not converted to Release
      to remote, because of still call route set to Bluetooth. When
      Call ends, BluetoothInCallService won't get callback of
      onCallAudioStateChanged, which is the reason why still stack
      is assuming call audio route is Bluetooth. So, Release didn't
      sent.
    
    - Fixed by setting call_audio_route_ to -1, when InCallservice
      sent update of callRemoved to BT stack.
    
    CRs-Fixed: 4056442
    Change-Id: I6344e05d78a19b764d67d247f397fd23f7bc159f
    (cherry picked from commit 485507d506b35810ffef0a8c4354e3735d93a0d3)

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -1304,6 +1304,11 @@ class LeAudioClientImpl : public LeAudioClient {
         defer_sink_suspend_ = false;
         OnLocalAudioSinkSuspend();
       }
+    } else {
+      if (!IsInCall()) {
+        log::debug(": call_audio_route_ not set to Bluetooth, as already Call ended.");
+        call_audio_route_ = -1;
+      }
     }
   }
 
@@ -1351,6 +1356,7 @@ class LeAudioClientImpl : public LeAudioClient {
       local_metadata_context_types_.source.clear();
       reconfigure = true;
     } else {
+      call_audio_route_ = -1;
       if (configuration_context_type_ == LeAudioContextType::CONVERSATIONAL) {
         log::info("Call is ended, speed up reconfiguration for media");
         local_metadata_context_types_ = in_call_metadata_context_types_;
@@ -1375,6 +1381,7 @@ class LeAudioClientImpl : public LeAudioClient {
         }
       } else {
         ReconfigureOrUpdateRemote(group, bluetooth::le_audio::types::kLeAudioDirectionSink);
+        UpdateCallAudioRoute(call_audio_route_);
       }
     }
   }
