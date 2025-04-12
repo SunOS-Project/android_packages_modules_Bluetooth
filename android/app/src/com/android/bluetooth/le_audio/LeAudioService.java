703010809d04f6cc6574b49f20d706e8846615d0
penghua <quic_penghua@quicinc.com>
2025-04-12 21:16:40 +0800

    Check incall status before update playing state
    Issue:
    1. An incoming CS call is over LEA
    2. DUT updated PLAYING state to remote
    3.The user fails to accpet the incoming call
    
    Fix:
    Change playing state to PAUSE if incall is true
    
    CRs-Fixed: 4113557
    
    Change-Id: I5a3269853ff6ca363979a63b698328f9fa0f079b

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -4313,7 +4313,9 @@ public class LeAudioService extends ProfileService {
 
         return descriptor.mInbandRingtoneEnabled;
     }
-
+    public boolean getInCall() {
+       return mInCall;
+    }
     /**
      * Set In Call state
      *
@@ -5755,7 +5757,18 @@ public class LeAudioService extends ProfileService {
             enforceBluetoothPrivilegedPermission(service);
             return service.groupAddNode(groupId, device);
         }
+        @Override
+        public boolean getInCall(AttributionSource source){
+            Objects.requireNonNull(source, "source cannot be null");
 
+            LeAudioService service = getService(source);
+            if (service == null) {
+                return false;
+            }
+
+            enforceBluetoothPrivilegedPermission(service);
+            return service.getInCall();
+        }
         @Override
         public void setInCall(boolean inCall, AttributionSource source) {
             Objects.requireNonNull(source, "source cannot be null");
