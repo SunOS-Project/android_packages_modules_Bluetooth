b6d8594d272150705b9df6cbdb7776319c6ba64e
wentjin <quic_wentjin@quicinc.com>
2025-07-07 11:58:58 +0800

    Mark IN_CALL_TRUE_UPDATE_FROM_BT_APP when group is not ready for CALL
    Testing case:
    1. Connect LE-A headset.
    2. Play music over LE-A.
    3. Make MO call, cancel immediately and initiate call again.
    Result: Sometimes call audio silent.
    
    Root cause: For this TC, stack will suspend MEDIA first then start VOICE. During this process, set call active again. If group is not
    streaming, it won't set IN_CALL_TRUE_UPDATE_FROM_BT_APP which regards
    it as stand-alone call finally.
    
    Fixed: In SetInCall function, it won't do reconfiguration when group
    is not streaming or target is streaming or it is under reconfiguration
    to other context. We should do reconfiguration for CONVERSATIONAL in below three scenarios.
    
    1. If group state is streaming.
    2. If group target state is streaming.
    3. If group is under reconfiguration, but not for conversational.
    
    Change-Id: I29c13f37236c87adbe937f31c05ac934eeedb60d
    CRs-Fixed: 4206762

--- system/bta/le_audio/client.cc
+++ system/bta/le_audio/client.cc
@@ -1297,8 +1297,12 @@ class LeAudioClientImpl : public LeAudioClient {
     }
 
     LeAudioDeviceGroup* group = aseGroups_.FindById(active_group_id_);
-    if (!group || !group->IsStreaming()) {
-      log::debug("{} is not streaming", active_group_id_);
+    //If group is under configuring/streaming to other context, it should do reconfiguration.
+    if (!group || (!group->IsStreaming() &&
+                    group->GetTargetState() != AseState::BTA_LE_AUDIO_ASE_STATE_STREAMING &&
+                    !(group->IsSuspendedForReconfiguration() &&
+                             configuration_context_type_ != LeAudioContextType::CONVERSATIONAL))) {
+      log::debug("{} is not streaming or not configuring to other contexts", active_group_id_);
       return;
     }
 
