9f3e6342e1a516cc85e361a43e93e0fa7e4e230c
Phaneendra Reddy <quic_phaneend@quicinc.com>
2025-07-15 11:51:36 +0530

    LEAudio: Dont Suspend LE when No active device of HFP is present
    When BT reset is done during call, leAudio suspend is called
    even before LE became active device. This is leading to no
    start from MM.
    When BT is reset, we are suspending LE audio only if headset
    active device is present.
    
    CRs-Fixed: 4214699
    Change-Id: Ic4165284276f31759fa86aea735962be02fd97e7

--- android/app/src/com/android/bluetooth/hfp/HeadsetService.java
+++ android/app/src/com/android/bluetooth/hfp/HeadsetService.java
@@ -2354,11 +2354,10 @@ public class HeadsetService extends ProfileService {
                    }
                 } else {
                   if (isAtLeastU()) {
-                      BluetoothDevice btDevice = mAdapterService.getActiveDeviceManager()
-                                                             .fetchLeAudioActiveDevice();
-                      if (btDevice == null) {
+                     if (mActiveDevice != null) {
+                         Log.i(TAG, "HFP active device is present. Setting LeAudiosuspend params");
                          mSystemInterface.getAudioManager().setLeAudioSuspended(true);
-                      }
+                     }
                   }
                 }
                 //Adding the wait mechanism Logic.
